16fae369681dc359aa3d621e38f2769df5543b80
Ralf Dannhauer <post@dannhauer.eu>
2014-06-27 22:49:39 +0200

    error from previous commit

--- Controller/TippsController.php
+++ Controller/TippsController.php
@@ -88,7 +88,7 @@ class TippsController extends AppController {
       // show actual round
       $this->Match->recursive = -1;
       $nextmatch = $this->Match->find('first', array(
-        'conditions' => array('Match.due >' => 'UNIX_TIMESTAMP()'), 
+        'conditions' => array('Match.due >' => time()), 
         'fields' => array('Match.round_id'),
         'order' => array('Match.due asc')));
       if (isset($nextmatch['Match']['round_id'])) {

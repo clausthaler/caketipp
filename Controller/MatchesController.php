9b9faa59e9c25ab45c7659a3b91d53184eb56e13
Ralf Dannhauer <post@dannhauer.eu>
2016-06-21 14:21:11 +0200

    no message

--- Controller/MatchesController.php
+++ Controller/MatchesController.php
@@ -498,7 +498,9 @@ class MatchesController extends AppController {
   }
 
   public function matchupdate($filename = null) {
-    if ($json = json_decode(file_get_contents('/var/www/push.tipp4fun.eu/' . $filename))) {
+    if ($json = json_decode(file_get_contents('/var/www/push.tipp4fun.eu/' . $filename)), true) {
+      $this->log('game ' . $json->{'Id'} . ' has begun');
+      die();
       if (substr($json->{'Updates'},0,15) == '[status:[TIMED:') {
         // game has begun -> set result to 0:0
         $checkmatch = $this->Match->findById($json->{'Id'} );
